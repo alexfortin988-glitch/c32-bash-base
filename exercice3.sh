143e8612c0002145cab2d435cb7d2a44e4944ae4
AlexF <alexfortin988@gmail.com>
2026-04-23 10:28:05 -0400

    cmt on ex3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+#Étape 1  = faire une focntion qui prend 1 param
+# Objectif - faire un nombre d'espaces en fonction du param
+#ex: faireEspace 40 -> echo "    "
+
+#Étape 2
+#Faire une boucle de 40 itérations, qui:
+# 1- Appelle faireEspace avec le $i
+
+
+
+# 2- Ajoutr le caractère @
+# 3-Ajoute un clear
+# 4- Ajouter un sleep de 0.1
+
+#Étape 3
+# sortie boucle, aficher BOOM
