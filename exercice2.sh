2fad5d48aee10f784851a5456da6aa51b8190171
AlexF <alexfortin988@gmail.com>
2026-04-23 10:21:22 -0400

    ex2

--- exercice2.sh
+++ exercice2.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 
+choix=
+
+while [[ $choix != "q" ]]
+do  
+    clear
+    echo "=========="
+    echo "- Menu 1 -"
+    #Afficher les options
+    echo "----------"
+    echo "-m  Exécute Exercice1.sh "
+    echo "-q Quitter "
+    read -p "Entrez un choix : " choix
+    #Si choix est m, appeler exercice1.sh
+    if [[ $choix = "m" ]]
+    then
+        read -p "Quel mot voulez-vous vérifier ?" mot
+
+        ./exercice1.sh $mot     # Vraiment as-is que dans le terminal
+        sleep 3
+    fi
+    
+    # Attendre 3 secondes
+done
