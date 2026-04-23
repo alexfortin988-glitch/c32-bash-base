ece17e9c7099a49ed7a7ca425d792eb67a715ca8
AlexF <alexfortin988@gmail.com>
2026-04-23 09:53:57 -0400

    08 fini

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo -n "Texte : " 
+read choix
+
+#read -p "Texte : " choix
+
+#optionnel, mais peu en avoir plusieurs
+#  + 1 à l'infini (au moins 1!!)
+if [[ $choix =~ ^[0-9]+$ ]]     # ~ -> motif de regex ; ^ -> début de ligne
+then    
+    echo "chiffre"
+fi
+
