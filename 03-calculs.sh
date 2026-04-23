7bf484206cf0a5c86278fde0861d1c3ab2fddb16
AlexF <alexfortin988@gmail.com>
2026-04-23 09:11:27 -0400

    03 Calcul

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo 10+10
+# Calcul archaique
+echo $((10+10))     # Sans espace !!!!!!!
+
+read -p "Entrez un nombre : " nb1
+read -p "Un autre nombre : " nb2
+
+#Exemple de calcul: Façon récente
+let resultat=$nb1*$nb2
+
+echo $resultat
