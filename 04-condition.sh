63ea0845b95916a0c3e026cf12d814030b85dbde
AlexF <alexfortin988@gmail.com>
2026-04-23 09:31:38 -0400

    Fin 04-conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+#========================================
+#Condtions
+note=61
+
+#Condition actuelle
+if [[ $note -lt 60 ]]
+then 
+    echo "Désolé, la note est insuffisante pour passer"
+
+# archaique 
+elif test $note -eq 60
+then
+    echo "C'est limite ...."
+else
+    echo "Bravo!"
+fi
+
+
+#=======================================================
+# Condition + évaluation d'une chaines de caractères
+read choix
+
+if [[ $choix != "a" ]];then
+    echo "Pourquoi ne pas avoir choisi a ??"
+    
+fi  
+
+
+
+#===================================================
+# Switch !
+
+case $choix in 
+  [[:lower:]])               #classe de caractère, il y en a plusieurs, voir en ligne
+     echo "lettre $choix en miniscule"
+    ;;
+  *) 
+    echo "pas en minuscule"
+    ;;
+esac
 
 
 
