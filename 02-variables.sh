e65a6736b8753173b78d549acbf4b8f2fa4173c9
AlexF <alexfortin988@gmail.com>
2026-04-23 09:07:21 -0400

    02-Variables complétées

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+prenom=John
+echo $prenom
+
+declare -i age=44           # numérique
+declare -r nom=Smithh     # lecture seule
+
+echo $age
+
+
+# Variable par défaut du fichier 
+echo $0     # Nom du fichier
+echo $1     # $1 à $9 = paramètre du script
+echo $#     # Combien de paramètre passés au script
+
